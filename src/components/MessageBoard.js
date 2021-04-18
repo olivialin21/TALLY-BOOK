25900ae986e1f2705393c5af8f6a06f659dc0d90
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-04-18 09:56:48 +0800

    save firestore ok

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,8 +1,8 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import "firebase/firestore";
 
 export default function MessageBoard() {
-  let firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyCeuaJfsj6jt3J2q9Q64P6On95fLSbgY4Q",
     authDomain: "react-mid-65a1a.firebaseapp.com",
     projectId: "react-mid-65a1a",
@@ -11,30 +11,30 @@ export default function MessageBoard() {
     appId: "1:289033592291:web:7650c109d31f8c4dd114d6",
     measurementId: "G-9RF0M985H3"
   };
-  // Initialize Firebase
+
   if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
   }
-  // firebase.analytics();
 
-  const email = document.getElementById("email");
-  const message = document.getElementById("message");
-  let docRef = firebase.firestore();
-  let messagesRef = docRef.collection("messageBoard");
-  
-  const onSubmit = () => {
-    let email_v = email.val();
-    let message_v = message.val();
-    
-    if (email_v!='' && message_v!=''){
+  const onClick = (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    let docRef = firebase.firestore();
+    let messagesRef = docRef.collection("messageBoard");
+
+    let email_v = email.value;
+    let message_v = message.value;
+
+    if (email_v !== '' && message_v!==''){
       messagesRef.add({
         "email":email_v,
         "message":message_v
+      }).then(() => {
+        document.getElementById("form").reset();
       });
-      email.val('');
-      message.val('');
-    }
-    else {
+    } else {
       alert('還有空格');
     }
   };
@@ -45,7 +45,7 @@ export default function MessageBoard() {
       <form id="form" className="messageForm">
         <input id="email" placeholder="e-mail..."/>
         <textarea id="message" placeholder="write something..." />
-        <input type="submit" onSubmit={onSubmit} value="Submit" className="messageForm-submit"/>
+        <input type="submit" onClick={onClick} value="Submit" className="messageForm-submit"/>
       </form>
     </div>
   );
